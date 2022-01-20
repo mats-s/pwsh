b2cce25555777b6791b79fad9e77854b41f4da47
mats-s <91266066+mats-s@users.noreply.github.com>
2022-01-20 12:44:57 +0100

    Update win-srv-setup.ps1

--- win-srv-setup.ps1
+++ win-srv-setup.ps1
@@ -2,7 +2,7 @@
 Get-WinUserLanguageList
 Set-WinUserLanguageList -LanguageList nb-NO
 
-#Choco install
+#Choco install og pwsh-core
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
