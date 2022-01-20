0276b4645df2857f105d3f46c5f82c2809df6c3f
mats-s <91266066+mats-s@users.noreply.github.com>
2022-01-20 13:24:44 +0100

    Update win-srv-setup.ps1

--- win-srv-setup.ps1
+++ win-srv-setup.ps1
@@ -1,25 +1,31 @@
+#cd
+cd C:\Users\Admin
+
+
 #Språk:
 Get-WinUserLanguageList
 Set-WinUserLanguageList -LanguageList nb-NO
 
-#Choco install og pwsh-core
+#Choco install
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y powershell-core
+exit
 
 #Sysinternals
 choco install -y sysinternals
 
 #BgInfo:
-curl -O https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip
+Invoke-WebRequest "https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip" -OutFile ".\LGPO.zip"  
 Expand-Archive .\LGPO.zip -DestinationPath LGPO
 cd .\LGPO\LGPO_30
-curl -O https://folk.ntnu.no/erikhje/mylgpo-13012022.zip
+Invoke-WebRequest "https://folk.ntnu.no/erikhje/mylgpo-13012022.zip" -OutFile ".\mylgpo-13012022.zip"  
 Expand-Archive .\mylgpo-13012022.zip -DestinationPath mylgpo
 .\LGPO.exe /g 'C:\Users\Admin\LGPO\LGPO_30\mylgpo\'
 
 #pwsh theme:
-curl -O https://codeload.github.com/lukesampson/concfg/zip/refs/heads/master
-Expand-Archive .\master -DestinationPath concfg
+cd C:\Users\Admin
+Invoke-WebRequest "https://codeload.github.com/lukesampson/concfg/zip/refs/heads/master" -OutFile ".\master.zip"
+Expand-Archive .\master.zip -DestinationPath concfg
 .\concfg\concfg-master\bin\concfg.ps1 import dracula -y
