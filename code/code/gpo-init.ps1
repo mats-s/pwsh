74b1f3a3aaf5c9e914d6f89a207350df4eae4bb2
Mats <matssta@stud.ntnu.no>
2022-03-17 14:51:24 +0100

    RM

--- code/code/gpo-init.ps1
+++
@@ -1,6 +0,0 @@
-# vi må forst laste ned 7zip for å kunne laste ned url filene
-choco install -y 7zip
-
-curl -O 
-7z x .\Windows%2010%20version%2021H2%20Security%20Baseline.zip
-cd .\Windows-10-v21H2-Security-Baseline\Scripts
